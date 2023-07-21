682d9755bd86cca58409f41d4c06626264747ae3
Martyna <132066674+M4rtM@users.noreply.github.com>
2023-07-21 22:17:04 +0200

    Small changesv2

--- ODT_SAS.m
+++ ODT_SAS.m
@@ -11,7 +11,7 @@ addpath(genpath(path));
 % 'Cell_Organelle';       %3D segmentation - cell + organelle
 
 
-type = 'Cell_Organelle';
+type = 'Cell';
 
 
 %% Load files
@@ -19,19 +19,19 @@ type = 'Cell_Organelle';
 
 %% Parameters for 3D cell segmentation
 %1) Preprocessing:
-thresh = 1.342; 
+thresh = 1.34; 
 strel_size = [];            %[2 2 2] or [3 3 3]
-option = 2;                 %option = 1 or option = 2; 
-noiseVolume = 2000;         %[um^3]; 
+option = 1;                 %option = 1 or option = 2; 
+noiseVolume = 500;         %[um^3]; 
 noiseVolume2 = 10000;       %[um^3] (if option == 2)
 
 %2)Seed points
-thinFragments = 1;          %set thinFragments to 1 if the analyzed biological cells 
+thinFragments = 0;          %set thinFragments to 1 if the analyzed biological cells 
                             %have complex shape containing thin fragments
                             %such as pseudopodia
 
 %3) Postprocessing:
-cellThresh=15000;           %[um^3];
+cellThresh=3000;           %[um^3];
 
 %% Parameters for 3D organelle segmentation
 LS = 1;                     %1 - lipid structure segmentation
@@ -43,9 +43,14 @@ cellCount = 1;              %number of cells analyzed (select only in 'Organelle
 switch type
     case 'Cell' 
        [maskCell, cellCount] = W3DRBM(REC, n_imm, dx, thresh, option, strel_size, noiseVolume, noiseVolume2, thinFragments, cellThresh);
-        masked = REC.*maskCell;
+        temp_MC = zeros(size(maskCell{1}));
+        for i=1:size(mask,1)
+            temp_MC = temp_MC + maskCell{i};
+        end
+        masked = REC.*temp_MC;
         File = strcat(Path,'maskCell.mat');
         save(File,'maskCell', '-v7.3')
+        koniec=toc/60;
 
     case 'Organelle'
         [mask,Path] = uigetfile('*.mat','Select cell mask');
